1d4d7cd64822a7999aa777b5ee55b0907f0aeb19
Filippo Gangi Dino <filippo.gangidino@welaika.com>
2013-05-24 17:35:35 +0200

    Remove Wordpress meta info from header and feeds

--- wp-laika-hacks.php
+++ wp-laika-hacks.php
@@ -49,8 +49,30 @@ function block(){
 }
 
 block();
-
 add_filter('login_url', 'add_key_to_url', 101, 2);
 add_filter('logout_url', 'add_key_to_url', 101, 2);
 add_filter('lostpassword_url', 'add_key_to_url', 101, 2);  
 add_filter('register', 'add_key_to_url', 101, 2);
+
+/*
+ * Remove Wordpress meta info from header and feeds
+ */
+
+//Remove generator name and version from your Website pages and from the RSS feed.
+add_filter('the_generator', create_function('', 'return "";'));
+//Display the XHTML generator that is generated on the wp_head hook, WP version
+remove_action( 'wp_head', 'wp_generator' ); 
+//Remove the link to the Windows Live Writer manifest file.
+remove_action('wp_head', 'wlwmanifest_link'); 
+//Remove EditURI
+remove_action('wp_head', 'rsd_link');
+//Remove index link.
+remove_action('wp_head', 'index_rel_link');
+//Remove previous link.
+remove_action('wp_head', 'parent_post_rel_link', 10, 0);      
+//Remove start link.
+remove_action('wp_head', 'start_post_rel_link', 10, 0);
+//Remove relational links (previous and next) for the posts adjacent to the current post.
+remove_action('wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0);
+//Remove shortlink if it is defined.
+remove_action('wp_head', 'wp_shortlink_wp_head', 10, 0);
