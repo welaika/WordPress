469b9e2db5392e0e4c16949bca75bf7983e12fbf
Filippo Gangi Dino <filippo.gangidino@welaika.com>
2013-05-27 17:49:35 +0200

    every plugins in pimp my wordpress

---
+++ wp-laika/plugins/pimp-my-wordpress/README.md
@@ -0,0 +1,24 @@
+# Pimp My WordPress
+
+Pimp My WordPress is a starting point for every WordPress we develop at 
+[weLaika](http://welaika.com). Give a list of plugin we usually install 
+on every installation, or we need to remember to install!
+
+## Plugin list
+
+WELAIKA PRODUCTS
+
+* [Wordless](http://wordpress.org/plugins/wordless/)
+* [Users to CSV](http://wordpress.org/plugins/users2csv/)
+
+OTHER
+
+* [Posts to Posts](http://wordpress.org/plugins/posts-to-posts/)
+* [Options Framework](http://wordpress.org/plugins/options-framework/)
+* [Simple Fields](http://wordpress.org/plugins/simple-fields/)
+* [InfiniteWP Client](http://wordpress.org/plugins/iwp-client/)
+* [White Label CMS](http://wordpress.org/plugins/white-label-cms/)
+* [Debug Bar](http://wordpress.org/plugins/debug-bar/)
+* [Debug Bar Console](http://wordpress.org/plugins/debug-bar-console/)
+* [Debug Bar Extender](http://wordpress.org/plugins/debug-bar-extender/)
+* [Formidable Forms](http://wordpress.org/plugins/formidable/)
---
+++ wp-laika/plugins/pimp-my-wordpress/pimp-my-wordpress.php
@@ -0,0 +1,50 @@
+<?php   
+  /* 
+  Plugin Name: Pimp My WordPress
+  Plugin URI: 
+  Description: Pimp My WordPress is a starting point for every WordPress we 
+    develop. Give a list of plugin we usually install on every installation, 
+    or we need to remember to install!
+  
+  Author: Edoardo Tenani, Filippo Gangi Dino, Welaika
+  Version: 0.2.0
+  Author URI: http://www.welaika.com
+  */  
+
+define("PMW_SITE_URL", get_bloginfo('url'));
+
+include_once ABSPATH . 'wp-admin/includes/plugin.php';
+
+$plugin_dir = dirname(__FILE__) ."/";
+$plugin_dirname = basename($plugin_dir);
+$plugin_url = plugins_url($plugin_dirname);
+
+require_once $plugin_dir . 'admin.php';
+require_once $plugin_dir . 'functions.php';
+
+# A list of plugin to be displayed and checked for installation
+/*
+  Example item:
+  + if name is equal to plugin slug, put only the plugin name:
+    'Wordless' 
+  + if plugin slug is different from name, create an array:
+    array(
+      'Name' => 'InfiniteWP Client',
+      'Slug' => 'iwp-client'
+    )
+
+    Please note: array keys are KEYS SENSITIVE!
+*/
+$pmw_to_be_installed_plugins = array(
+  'Wordless',
+  'users2Csv',
+  'White Label CMS', 
+  array('Name' => 'InfiniteWP Client', 'Slug' => 'iwp-client'), 
+  'Simple Fields', 
+  'Options Framework', 
+  'Posts To Posts',
+  'Debug Bar',
+  'Debug Bar Console',
+  'Debug Bar Extender',
+  'Formidable Forms',
+);
